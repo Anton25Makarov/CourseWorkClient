7243b5bf9d35bd6c7a9f3cc51e0ce42af0a1e165
Anton Makarov <nbsketmn250799@gmail.com>
2018-11-14 23:47:12 +0300

    add authorisation

--- src/main/java/by/bsuir/Main.java
+++ src/main/java/by/bsuir/Main.java
@@ -1,18 +1,14 @@
 package by.bsuir;
 
-import by.bsuir.client.Connection;
+import by.bsuir.window.AuthorisationWindow;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Main {
 
-    public static void main(String[] args){
-        Connection connection = new Connection();
+    public static void main(String[] args) {
+        AuthorisationWindow authorisationWindow = new AuthorisationWindow();
+        authorisationWindow.setVisible(true);
+        authorisationWindow.setLocationRelativeTo(null);
+//        Connection connection = new Connection();
     }
 }
---
+++ src/main/java/by/bsuir/window/AuthorisationWindow.java
@@ -0,0 +1,111 @@
+package by.bsuir.window;
+
+import by.bsuir.entities.User;
+
+import javax.swing.*;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class AuthorisationWindow extends JFrame {
+
+    private JLabel loginLabel;
+    private JLabel passwordLabel;
+    private JTextField login;
+    private JPasswordField password;
+    private JButton ok;
+    private JPanel panel;
+    private MenuAdminWindow menuAdminWindow;
+
+    private Socket socket;
+    private ObjectOutputStream objectOutputStream;
+    private ObjectInputStream objectInputStream;
+
+
+    public AuthorisationWindow() {
+        super("Авторизация");
+        setSize(300, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        loginLabel = new JLabel("Логин: ");
+        loginLabel.setLocation(50, 50);
+        loginLabel.setSize(75, 50);
+
+        login = new JTextField();
+        login.setLocation(110, 60);
+        login.setSize(100, 30);
+
+        passwordLabel = new JLabel("Пароль: ");
+        passwordLabel.setLocation(50, 100);
+        passwordLabel.setSize(75, 50);
+
+        password = new JPasswordField();
+        password.setLocation(110, 110);
+        password.setSize(100, 30);
+
+        ok = new JButton("Ok");
+        ok.setLocation(90, 160);
+        ok.setSize(100, 30);
+
+        panel = new JPanel();
+        panel.setLayout(null);
+        panel.add(loginLabel);
+        panel.add(login);
+        panel.add(password);
+        panel.add(passwordLabel);
+        panel.add(ok);
+
+        add(panel);
+
+        ok.addActionListener(event -> {
+            try {
+                if (login.getText().isEmpty() || String.copyValueOf(password.getPassword()).isEmpty()) {
+                    JOptionPane.showMessageDialog(this,
+                            "Необходимо ввести логин и пароль");
+                    return;
+                }
+                createSocket();
+                if (socket == null) {
+                    return;
+                }
+                objectOutputStream.writeObject("authorisation");
+
+                User user = new User(login.getText(), String.copyValueOf(password.getPassword()));
+
+                objectOutputStream.writeObject(user);
+
+                String answer = (String) objectInputStream.readObject();
+                if (answer.equals("true")) {
+                    JOptionPane.showMessageDialog(this,
+                            "Красава");
+                } else {
+                    JOptionPane.showMessageDialog(this,
+                            "Неверно указаны логин или пароль");
+                }
+
+            } catch (EOFException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void createSocket() {
+        try { // установка соединения с сервером
+            if (socket == null || objectOutputStream == null || objectInputStream == null) {
+                socket = new Socket(InetAddress.getLocalHost(), 8071);
+                objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                objectInputStream = new ObjectInputStream(socket.getInputStream());
+            }
+        } catch (ConnectException e) {
+            JOptionPane.showMessageDialog(this, "Не удалось подключиться к серверу");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
